b298dc48b54f0073e51ad0c4d9416660e4e71394
Alexyus <hirzoiualex@yahoo.com>
2020-05-18 16:41:53 +0300

    Init
    Change-Id: I58e5180095cece6b4ac59e78f72da8f0e27c2894

---
+++ pluralsigh-springcloud-m2-configserver-git/src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("admin").build());
+        return manager;
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	 http.csrf().disable().authorizeRequests()
+         .anyRequest().authenticated().and()
+         .httpBasic();
+ 
+    }
+}
